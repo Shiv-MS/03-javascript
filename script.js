1eb5246bedec210633c20280cec73d4b2b70eec0
Shivam Shewa <shewashivam@gmail.com>
2020-10-08 03:15:56 -0500

    input should be validated and at least one character type should be selected

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-// Assignment Code
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+function validate_inputs(prompts_from_user) {
+  if (prompts_from_user.password_length > 8 && prompts_from_user.password_length < 128) {
+    console.log("Length is valid")
+  }
+  // Given a user has answered all of the questions.
+  // Make sure they have followed then following crieteria....
+  if (Object.values(prompts_from_user).indexOf(true) > -1) {
+    console.log("User input validated")
+    gen_pass(prompts_from_user)
+  }
+}
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
