935138c2783ba7a167d07de36990288b15eacf0f
Shivam Shewa <shewashivam@gmail.com>
2020-10-08 03:17:51 -0500

    password is generated that matches the selected criteria

--- script.js
+++ script.js
@@ -13,6 +13,27 @@ function validate_inputs(prompts_from_user) {
   }
 }
 
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+function gen_pass(crieteria) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const alphabet_cap = alphabet.map(letter => letter.toUpperCase());
+
+  // Make an array of special chars from String
+  const special_characters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+
+  const pass_array = [];
+
+
+  // Create a loop that goes until it hits the password_length and creates an array
+  for (i = 0; i < prompts_from_user.password_length; i++) {
+    var index = Math.floor(Math.random() * prompts_from_user.password_length);
+    var digit = passArray[index];
+    results.push(pass_array);
+  }
+  // Flatten the array
+  console.log(special_characters)
+}
 
 var generateBtn = document.querySelector("#generate");
 
